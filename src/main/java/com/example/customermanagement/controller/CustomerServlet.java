15c92036e1cdc57d515b42a4d5a2e5ea97cf7d4e
tuan anh <tuananhtran.xd@gmail.com>
2024-02-01 15:16:32 +0700

    first commit

---
+++ src/main/java/com/example/customermanagement/controller/CustomerServlet.java
@@ -0,0 +1,132 @@
+package com.example.customermanagement.controller;
+
+import com.example.customermanagement.model.Customer;
+import com.example.customermanagement.service.CustomerService;
+import com.example.customermanagement.service.CustomerServiceImpl;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.List;
+
+@WebServlet(name = "customerServlet", urlPatterns = "/customers")
+public class CustomerServlet extends HttpServlet {
+    private final CustomerService customerService = new CustomerServiceImpl();
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+        String action = req.getParameter("action");
+        if (action == null) {
+            action = "";
+        }
+        switch (action) {
+            case "create":
+                createNewCustomer(req, resp);
+                break;
+            case "edit":
+                updateCustomer(req, resp);
+                break;
+            case "delete":
+//                deleteCustomer(request, response);
+                break;
+            default:
+                break;
+        }
+    }
+
+    private void updateCustomer(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        int id = Integer.parseInt(req.getParameter("id"));
+        String name = req.getParameter("name");
+        String email = req.getParameter("email");
+        String address = req.getParameter("address");
+        Customer customer = this.customerService.findById(id);
+        RequestDispatcher dispatcher;
+
+        if (customer == null) {
+            dispatcher = req.getRequestDispatcher("error-404.jsp");
+        } else {
+            customer.setName(name);
+            customer.setEmail(email);
+            customer.setAddress(address);
+            this.customerService.update(id, customer);
+            req.setAttribute("customer", customer);
+            req.setAttribute("message", "Customer information was updated");
+            dispatcher = req.getRequestDispatcher("customer/edit.jsp");
+//            resp.sendRedirect("/customers");
+        }
+        dispatcher.forward(req, resp);
+//
+    }
+
+    private void createNewCustomer(HttpServletRequest req, HttpServletResponse resp) {
+        String name = req.getParameter("name");
+        String email = req.getParameter("email");
+        String address = req.getParameter("address");
+        int id = customerService.findAll().size()+1;
+        Customer customer = new Customer(id, name, email, address);
+        customerService.save(customer);
+        try {
+            resp.sendRedirect("/customers");
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String action = req.getParameter("action");
+        if (action == null){
+            action = "";
+        }
+        switch (action){
+            case "create":
+                showPageCreate(req, resp);
+                break;
+            case "edit":
+                showPageEdit(req, resp);
+                break;
+            default:
+                showAllCusomers(req, resp);
+                break;
+        }
+
+    }
+
+    private void showPageEdit(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        int id = Integer.parseInt(req.getParameter("id"));
+        Customer customer = this.customerService.findById(id);
+        RequestDispatcher dispatcher;
+
+        if (customer == null) {
+            dispatcher = req.getRequestDispatcher("error-404.jsp");
+        } else {
+            req.setAttribute("customer", customer);
+            dispatcher = req.getRequestDispatcher("customer/edit.jsp");
+        }
+            dispatcher.forward(req, resp);
+    }
+
+    private void showPageCreate(HttpServletRequest req, HttpServletResponse resp) {
+        RequestDispatcher requestDispatcher = req.getRequestDispatcher("customer/create.jsp");
+        try {
+            requestDispatcher.forward(req, resp);
+        } catch (ServletException e) {
+            throw new RuntimeException(e);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private void showAllCusomers(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        RequestDispatcher requestDispatcher = req.getRequestDispatcher("customer/list.jsp");
+        List<Customer> customerList = customerService.findAll();
+        req.setAttribute("ct", customerList);
+        requestDispatcher.forward(req, resp);
+    }
+}
---
+++ src/main/webapp/customer/list.jsp
@@ -0,0 +1,40 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: admin
+  Date: 2/1/2024
+  Time: 11:28 AM
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<%@taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
+<html>
+<head>
+    <title>Customer List</title>
+</head>
+<body>
+<h1>Customers</h1>
+<p>
+    <a href="/customers?action=create">Create new customer</a>
+</p>
+<table border="1">
+    <tr>
+        <td>ID</td>
+        <td>Name</td>
+        <td>Email</td>
+        <td>Address</td>
+        <td>Edit</td>
+        <td>Delete</td>
+    </tr>
+    <c:forEach  var="customer" items="${ct}">
+        <tr>
+            <td>${customer.id}</td>
+            <td>${customer.name}</td>
+            <td>${customer.email}</td>
+            <td>${customer.address}</td>
+            <td><a href="/customers?action=edit&id=${customer.getId()}">edit</a></td>
+            <td><a href="/customers?action=delete&id=${customer.getId()}">delete</a></td>
+        </tr>
+    </c:forEach>
+</table>
+</body>
+</html>
